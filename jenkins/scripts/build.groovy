94657a5ccfe755ada296ef3e214426c4a7ada60e
Romei, Simone <simone.romei@sap.com>
2016-12-13 19:01:34 +0100

    no message

--- jenkins/scripts/build.groovy
+++ jenkins/scripts/build.groovy
@@ -2,7 +2,7 @@
 
 def unzipPlatform(hybrisZip, destFolder) {
 	echo "unzipping ${hybrisZip} to ${destFolder}"
-	sh 'mkdir ${destFolder}'
-	sh 'unzip ${hybrisZip} -d ${destFolder}'
+	sh "mkdir ${destFolder}"
+	sh "unzip ${hybrisZip} -d ${destFolder}"
 }
 return this
