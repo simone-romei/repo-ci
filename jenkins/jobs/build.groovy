98b528adab128905ff5a4a1254b055f06be79df6
simone romei <simone.romei@gmail.com>
2021-01-31 22:16:01 +0100

    .

--- jenkins/jobs/build.groovy
+++ jenkins/jobs/build.groovy
@@ -1,23 +1,7 @@
 
 job("commerce - build") {
 
-    parameters {
-
-        gitParam('commerce_git') {
-            
-            branch('commerce_git_branch')
-        
-            defaultValue('default')
-        
-            description('Commerce Repository')
-            // Specifies the sort order for tags.
-            //sortMode(String sortMode)
-            // Specifies a filter for tags.
-            //tagFilter(String tagFilter)
-            // Specifies the type of selectable values.
-            //type(String type)
-        }
-
-    }
+    description('Build the commerce platform')
+    
 
 }
