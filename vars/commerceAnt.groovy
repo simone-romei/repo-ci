bfea362e11a74c53fc1ad66f3cd95e8777dce790
simone romei <simone.romei@gmail.com>
2021-02-02 00:19:25 +0100

    .

--- vars/commerceAnt.groovy
+++ vars/commerceAnt.groovy
@@ -1,6 +1,5 @@
 
 /* groovylint-disable-next-line MethodReturnTypeRequired, NoDef */
 def call(String task) {
-    /* groovylint-disable-next-line GStringExpressionWithinString */
-    echo 'commerceAnt ${task}'
+    echo "commerceAnt ${task}"
 }
