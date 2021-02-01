1952245a6f5bd731f890295d1df650f2045c764b
simone romei <simone.romei@gmail.com>
2021-02-01 11:43:47 +0100

    .

--- seed/jobs/build.groovy
+++ seed/jobs/build.groovy
@@ -2,5 +2,7 @@
 job('commerce-build') {
     description('build commerce platform')
     jdk('Java 11')
-    cloneWorkspace('commerce-workspace', 'Any')
+    scm {
+        cloneWorkspace('commerce-workspace', 'Any')
+    }
 }
--- seed/jobs/workspace.groovy
+++ seed/jobs/workspace.groovy
@@ -2,6 +2,6 @@
 job('commerce-workspace') {
     description('prepare the workspace for commerce')
     parameters {
-        fileParam('commerce-suite/hybris-commerce-suite-2005.0.zip', 'SAP Commerce Suite to be used')
+        fileParam('commerce-suite/hybris-commerce-suite.zip', 'SAP Commerce Suite to be used')
     }
 }
