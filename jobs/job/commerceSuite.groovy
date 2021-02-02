3c8b35d048794735063be463f0700b3e24a3c653
simone romei <simone.romei@gmail.com>
2021-02-02 01:10:31 +0100

    .

--- jobs/job/commerceSuite.groovy
+++ jobs/job/commerceSuite.groovy
@@ -1,9 +1,9 @@
 job('commerce-suite') {
     description('Job for peparing a commerce workspace')
     parameters {
-        fileParam('commerce-suite/hybris-commerce-suite.zip', 'SAP Commerce Suite')
+        fileParam('hybris-commerce-suite.zip', 'SAP Commerce Suite')
     }
     steps {
-        shell("unzip -q commerce-suite/hybris-commerce-suite.zip -d commerce-suite")
+        shell("unzip -q hybris-commerce-suite.zip -d commerce-suite")
     }
 }
